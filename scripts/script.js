f0d600a190f82d12dca283b94b7ad8712820c757
Grat10borg <oscarblybhansen@gmail.com>
2022-08-21 19:34:35 +0200

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+"use strict";
+//#region Random title selector 
+// array for document titles
+let DocTitlenames = Array("Website of Mother1Brain", "Data Central: Brain Mother", "Mother-Data: Brain Central", "Brain Central: Data Mother", "undefined-Brain: undefined Data", "Mom-brain: Integer Control", "Mother-brain: Data Central", "Are you reloading the page?", "Data Mother: Central Brain", "Central: Data Brain Mother", "Brain Data: Mother Central", "YT: 'watch?v=DLzxn1SgKds' Watch..");
+// picks a random title from DocTitlenames and sets it as the Title of the document
+document.title = DocTitlenames[Math.floor(Math.random() * DocTitlenames.length)];
+//#endregion
+//#region Favicon Selector
+let faviconSrcs = Array("MotherBrainIconGrat.png", "MotherBrainIconChar.png", "MotherBrainIconCRT.png", "MotherBrainIconIllu.png", "MotherBrainIconTessa.png");
+let favicon = document.getElementById("Favicon");
+favicon.setAttribute("href", `.\\styling\\icon\\${faviconSrcs[Math.floor(Math.random() * faviconSrcs.length)]}`);
+//#endregion

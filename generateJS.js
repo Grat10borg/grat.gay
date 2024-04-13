541466f050165e7c70b0abd9dbd91f1e92b86bf1
Grat <109081987+Grat10borg@users.noreply.github.com>
2024-04-13 16:06:11 +0200

    images now sorted newest to oldest

--- generateJS.js
+++ generateJS.js
@@ -22,28 +22,39 @@ function GenerateJSFile(artFolders) {
 		let folderContent = [];
 		let imageCreationDate = [];
 
+		var files = fs.readdirSync(folder);
+
+		console.log("unsorted");
+		console.log(files);
+			/* sorts files by oldest to newest*/
+		files.sort((a, b) => {
+			return fs.statSync(folder +"/"+ a).mtime.getTime() -
+				fs.statSync(folder +"/"+ b).mtime.getTime();
+		});
+			/* reverse sorting, newest to oldest. */
+		files = files.reverse();
+		console.log("sorted");
+		console.log(files);
+
 		// read all files inside the art folders 
-		fs.readdirSync(folder).forEach(file => {
+		//fs.readdirSync(folder).forEach(file => {
 			/* sort newest to oldest */
 
-			fs.stat(folder+"/"+file, (err, stats) => {
-				if(err) console.log(err);
-				else {
-					// logs 2024-04-10T19:40:19.661Z 
-					console.log(stats.birthtimeMs);
+		//	fs.stat(folder+"/"+file, (err, stats) => {
+		//		if(err) console.log(err);
+		//		else {
+		//			// logs 2024-04-10T19:40:19.661Z 
+		//			console.log(stats.birthtimeMs);
 
-					imageCreationDate.push(stats.birthtimeMs);
-				}
-			});
-			folderContent.push(file);
-		});
+		//			imageCreationDate.push(stats.birthtimeMs);
+		//		}
+		//	});
+		//	folderContent.push(file);
+		//});
 	
-		/* outputs [] */
-		console.log(imageCreationDate);
-		
 		// create JS arrays with folders images
 		file += "let "+foldername[3]+" = "
-		+JSON.stringify(folderContent)+"; \n";
+		+JSON.stringify(files)+"; \n";
 	});
 	
 	// export art JS file to the art folder

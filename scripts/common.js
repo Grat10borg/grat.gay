4140446d55937c49c52e27e9985f295ff0fb9e6b
Grat <109081987+Grat10borg@users.noreply.github.com>
2025-09-27 16:43:58 +0200

    second try!

--- scripts/common.js
+++ scripts/common.js
@@ -400,7 +400,7 @@ if($$.id("friends")) {
 // for blog page
 // for loading blog pages from hashtag
 if($$.id("manga")) { blogPlace(); }
-window.addEventListener("hashchange", blogPlace() );
+window.addEventListener("hashchange", window.refresh() );
 
 async function blogPlace() {
     for(post in images["blog"]) {
